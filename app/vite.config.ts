1472c4588126b6acb0fcf625c07a2293962f2b74
Rodrigo Borges <rodrigo-b1@outlook.com>
2025-06-08 14:14:47 -0300

    feat: ajustes no build e melhorias no projeto

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // importante para deploy no Vercel
+  base: '/',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
