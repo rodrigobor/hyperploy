1a3f75b965c545dacbe61aaadc1585a6d38f96c8
Rodrigo Borges <rodrigo-b1@outlook.com>
2025-06-08 19:44:07 -0300

    Fix SPA routing for Vercel

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  build: {
-    outDir: 'dist',
-  },
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, 'src'),
     },
+  },
+  build: {
+    outDir: 'dist', // necessário para Vercel
   },
 })
